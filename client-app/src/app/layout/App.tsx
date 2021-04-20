812716ad6db6eb0a93c77fb9cfb53a4460e498a5
Vruyr <vruyror@ogmainc.com>
2021-04-20 12:06:34 +0400

    change 1234

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import './styles.css';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/Activities').then(response => {
+      setActivities(response.data);
+    })
+  }, []);
+
+  return (
+    <Fragment>
+        <NavBar />
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard activities={activities} />
+        </Container>
+    </Fragment>
+  );
+}
+
+export default App;
