144dcee8e7603b0ceb76b789bbd846be41418ab2
Vruyr <vruyror@ogmainc.com>
2021-06-01 18:03:56 +0400

    Security Headers added2

--- API/Startup.cs
+++ API/Startup.cs
@@ -74,6 +74,7 @@ namespace API
                 //app.UseHsts();
                 app.Use(async (context, next) => {
                     context.Request.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
                 });
             }
 
