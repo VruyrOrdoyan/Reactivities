c1ed36cb677e1b8fbef895eaf7ddf37aee65257d
Vruyr <vruyror@ogmainc.com>
2021-06-05 13:04:02 +0400

    Added style hash

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,7 +58,9 @@ namespace API
             app.UseXfo(opt => opt.Deny());
             app.UseCsp(opt => 
                 opt.BlockAllMixedContent()
-                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-oFySg82XYSNiSd+Q3yfYPD/rxY6RMDMJ0KxzGG74iGM="))
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"
+                    , "sha256-oFySg82XYSNiSd+Q3yfYPD/rxY6RMDMJ0KxzGG74iGM="
+                    , "sha256-iv4u281ryDFvLnNHPRS71LdIH1GQfkYMSMdyLM24i2M="))
                 .FormActions(s => s.Self())
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FrameAncestors(s => s.Self())
